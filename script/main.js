8320b8187a210e65198e1b5f91cd6f7e9d7ff5fe
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-08-26 18:31:09 +0200

    added loading before animation start

--- script/main.js
+++ script/main.js
@@ -26,31 +26,64 @@ if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
   canvas.height = x * 9;
 }
 
+const bubbleFour = document.querySelector(".buttonWrapper");
+const bubbleOne = document.querySelector(".bubbleOne");
+const bubbleTwo = document.querySelector(".bubbleTwo");
+const bubbleThree = document.querySelector(".bubbleThree");
+const button = document.querySelector("button");
+const heading = document.querySelector("h1");
+const aboutLink = document.querySelector(".aboutLink");
+
+function startAnimation() {
+  heading.classList.add("fadeInH1");
+  bubbleOne.classList.add("bubbleOneFade");
+  bubbleTwo.classList.add("bubbleTwoFade");
+  bubbleThree.classList.add("bubbleThreeFade");
+  bubbleFour.classList.add("buttonWrapperFade");
+  aboutLink.classList.add("fadeInAboutLink");
+}
+
+function enableStartButton() {
+  const startScreen = document.querySelector(".startScreen");
+
+  bubbleFour.style.cursor = "pointer";
+  button.style.cursor = "pointer";
+
+  bubbleFour.addEventListener("click", () => {
+    startScreen.classList.add("fadeOut");
+    setTimeout(function () {
+      startScreen.style.display = "none";
+    }, 900);
+  });
+}
+
+function removeHidden() {
+  const elements = document.querySelectorAll(".hidden");
+  elements.forEach((element) => {
+    setTimeout(function () {
+      element.classList.remove("hidden");
+      enableStartButton();
+    }, 1550);
+  });
+}
+
+const loadingBar = document.querySelector("#unity-loading-bar");
+const progressBarFull = document.querySelector("#unity-progress-bar-full");
+loadingBar.style.display = "block";
+
 const script = document.createElement("script");
 script.src = loaderUrl;
 script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {})
+  createUnityInstance(canvas, config, (progress) => {
+    progressBarFull.style.width = 100 * progress + "%";
+  })
     .then((unityInstance) => {
-      console.log("loading");
+      loadingBar.style.display = "none";
+      startAnimation();
+      removeHidden();
     })
     .catch((message) => {
       alert(message);
     });
 };
 document.body.appendChild(script);
-
-const playButton = document.querySelector(".buttonWrapper");
-const startScreen = document.querySelector(".startScreen");
-playButton.addEventListener("click", () => {
-  startScreen.classList.add("fadeOut");
-  setTimeout(function () {
-    startScreen.style.display = "none";
-  }, 900);
-});
-
-const elements = document.querySelectorAll(".hidden");
-elements.forEach((element) => {
-  setTimeout(function () {
-    element.classList.remove("hidden");
-  }, 1550);
-});
