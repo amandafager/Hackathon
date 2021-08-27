df88d2687f56939d373db529b3d3da5f5e339034
amanda.fager@outlook.com <amanda.fager@outlook.com>
2021-08-27 11:06:37 +0200

    avatars and metadata

--- script/about.js
+++ script/about.js
@@ -1,6 +1,6 @@
 const cardItems = [
   {
-    avatar: "",
+    avatar: "../images/avatars/Profil_Amanda.png",
     name: "Amanda Fager",
     education: "Webbutvecklare",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium orci sem, in rutrum nisi bibendum vitae. Sed suscipit ac metus in rutrum.",
@@ -8,7 +8,7 @@ const cardItems = [
     linkTitle: "github.com/amandafager",
   },
   {
-    avatar: "",
+    avatar: "../images/avatars/Profil_Josefin.png",
     name: "Josefin Ramsberg",
     education: "Game Creator Artist",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium orci sem, in rutrum nisi bibendum vitae. Sed suscipit ac metus in rutrum.",
@@ -16,7 +16,7 @@ const cardItems = [
     linkTitle: "",
   },
   {
-    avatar: "",
+    avatar: "../images/avatars/Profil_Johanna.png",
     name: "Johanna Smitz",
     education: "Manusförfattare",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium orci sem, in rutrum nisi bibendum vitae. Sed suscipit ac metus in rutrum.",
@@ -24,7 +24,7 @@ const cardItems = [
     linkTitle: "",
   },
   {
-    avatar: "",
+    avatar: "../images/avatars/Profil_Moa.png",
     name: "Moa Erikson",
     education: "UX Designer",
     text: "Som gruppens UX-designer har jag bland annat tagit ansvar för att få igång idégeneringen samt bygga upp en visuell prototyp som vi kan utgå från under projektets gång. Jag har arbetat med text och färg där enhetlighet och temat kärlek har varit i fokus. Jag har också under hela projektets gång arbetat för användarvänlighet och för att spelet ska ge en tillfredsställande upplevelse. ",
@@ -32,7 +32,7 @@ const cardItems = [
     linkTitle: "",
   },
   {
-    avatar: "",
+    avatar: "../images/avatars/Profil_Nicklas.png",
     name: "Nicklas Hidesjö",
     education: "Game Creator Programmer",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium orci sem, in rutrum nisi bibendum vitae. Sed suscipit ac metus in rutrum.",
@@ -41,18 +41,17 @@ const cardItems = [
   },
 ];
 
-{
-  /* <img src="${avatar}" alt="Avatar" style="width: 100%" /> */
-}
-
 function createCard(avatar, name, education, text, link, linkTitle) {
   let template = `
       <div class="card">
-          <h3>${name}</h3>
-          <h4>${education}</h4>
-          <div class="line"></div>
-          <p>${text}</p>
-          <a href="${link}">${linkTitle}</a>
+        <div class="imgWrapper">
+          <img class="avatar" src="${avatar}" alt="Avatar"/>
+        </div>
+        <h3>${name}</h3>
+        <h4>${education}</h4>
+        <div class="line"></div>
+        <p>${text}</p>
+        <a href="${link}">${linkTitle}</a>
       </div>`;
   return template;
 }
